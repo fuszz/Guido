0718fe0e1ec3b92a598bb031af08ae7866324990
fuszz <uliaszfilip@gmail.com>
2023-08-17 15:07:10 +0200

    Obsługa wczytywania plików wejściowych (17.08.2023)

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+import obsluga_plikow
+import partytura
 
+obsluga_plikow.odczytuj_plik("partytura_1.txt")
 
--- obsluga_plikow.py
+++ obsluga_plikow.py
@@ -2,56 +2,81 @@ import akord
 import dzwiek
 import partytura
 import tonacja
+import string
+from typing import TextIO
 
 
-class ObslugaPLikow:
+class BladWNaglowku(Exception):
+    def __init__(self):
+        self.wiadomosc: str = "Błąd w nagłówku pliku. Czy wejście jest poprawne?"
 
-    @staticmethod
-    def odczytuj_plik(sciezka_do_pliku: str) -> partytura.Partytura:
+    def __str__(self):
+        return self.wiadomosc
 
-        licznik: int = 0
 
-        with open(sciezka_do_pliku, "r") as dane:
-            for linia in dane:
-                linia.replace(" ", "")
-                # Nagłówek nowotworzonej partytury
-                if linia:
-                    # Kiedy licznik jest mniejszy od 3, zbieram parametry tworzonej partytury
-                    if licznik < 3:
-                        licznik += 1
-                        parametr, wartosc = linia.split(":")
+class BladWCiele(Exception):
+    def __init__(self):
+        self.wiadomosc: str = "Błąd w ciele. Czy wejście jest poprawne?"
 
-                        if parametr == "metrum":
-                            nowe_metrum: str = wartosc.replace(" ", "")
-                        elif parametr == "tonacja":
-                            nazwa_tonacji: str = wartosc.replace(" ", "")
-                        elif parametr == "takty":
-                            nowa_liczba_taktow: int = int(wartosc)
+    def __str__(self):
+        return self.wiadomosc
 
-                        if licznik == 3: #Tworzenie partytury
-                            licznik += 1
-                            nowa_tonacja = tonacja.Tonacja(nazwa_tonacji.strip())
-                            nowa_partytura = partytura.Partytura(nowa_tonacja, nowe_metrum.strip(), nowa_liczba_taktow)
 
-                    else:
-                        linia = linia.strip()
-                        if linia == "T":
-                            nowa_partytura.zakoncz_takt()
-                        else:
+def utworz_partyture(plik: TextIO) -> partytura.Partytura:
+    try:
+        nowe_metrum: str = None
+        nowa_tonacja: tonacja.Tonacja = None
+        nowa_liczba_taktow: int = None
+        while nowe_metrum is None or nowa_tonacja is None or nowa_liczba_taktow is None:
+            linia = plik.readline()
+            (klucz, wartosc) = linia.split(":")
+            klucz = klucz.replace('\n', '').replace(' ', '')
+            wartosc = wartosc.replace('\n', '').replace(' ', '')
+            if klucz == "metrum":
+                nowe_metrum = wartosc
+            elif klucz == "tonacja":
+                nowa_tonacja = tonacja.Tonacja(wartosc)
+            elif klucz == "takty":
+                nowa_liczba_taktow = int(wartosc)
+        if nowe_metrum is not None and nowa_tonacja is not None and nowa_liczba_taktow is not None:
+            return partytura.Partytura(nowa_tonacja, nowe_metrum, nowa_liczba_taktow)
+        else:
+            raise BladWNaglowku
+    except:
+        raise BladWNaglowku
 
-                            dzwieki = linia.split(",")
-                            d_s, d_a, d_t, d_b, wartosc = dzwieki
 
-                            d_s = d_s.strip()
-                            d_a = d_a.strip()
-                            d_t = d_t.strip()
-                            d_b = d_b.strip()
-
-                            wartosc = float(wartosc)
-                            dzwiek_sopranu = dzwiek.Dzwiek(int(d_s[-1]), d_s[:-1])
-                            dzwiek_altu = dzwiek.Dzwiek(int(d_a[-1]), d_a[:-1])
-                            dzwiek_tenoru = dzwiek.Dzwiek(int(d_t[-1]), d_t[:-1])
-                            dzwiek_basu = dzwiek.Dzwiek(int(d_b[-1]), d_b[:-1])
-                            nowy_akord = akord.Akord(dzwiek_sopranu, dzwiek_altu, dzwiek_tenoru, dzwiek_basu, wartosc)
-                            nowa_partytura.dodaj_akord(nowy_akord)
+def wypelnij_partyture_akordami(plik: TextIO, nowa_partytura: partytura.Partytura) -> partytura.Partytura:
+    try:
+        for linia in plik:
+            linia = linia.replace(' ', '').replace('\n', '')
+            if linia == "T":
+                nowa_partytura.zakoncz_takt()
+            else:
+                podane_dzwieki = linia.split(',')
+                dzwiek_sopranu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[0][-1]), podane_dzwieki[0][0:-1].strip())
+                dzwiek_altu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[1][-1]), podane_dzwieki[1][0:-1].strip())
+                dzwiek_tenoru: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[2][-1]), podane_dzwieki[2][0:-1].strip())
+                dzwiek_basu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[3][-1]), podane_dzwieki[3][0:-1].strip())
+                nowy_akord: akord.Akord = akord.Akord(dzwiek_sopranu, dzwiek_altu, dzwiek_tenoru, dzwiek_basu,
+                                                  float(podane_dzwieki[4]))
+                nowa_partytura.dodaj_akord(nowy_akord)
         return nowa_partytura
+    except:
+        raise BladWCiele
+
+
+def odczytuj_plik(sciezka_do_pliku: str) -> partytura.Partytura:
+    try:
+        with open(sciezka_do_pliku, "r") as plik_wejsciowy:
+            nowa_partytura: partytura.Partytura = utworz_partyture(plik_wejsciowy)
+            nowa_partytura = wypelnij_partyture_akordami(plik_wejsciowy, nowa_partytura)
+            if not nowa_partytura.czy_poprawna_liczba_taktow():
+                raise ValueError("Blednie podane akordy")
+        return nowa_partytura
+    except BladWNaglowku:
+        print("Błędny nagłówek pliku wejściowego. Niemożliwe utworzenie partytury.")
+        raise ValueError("Niepoprawne wczytanie danych. Problem z nagłówkiem pliku.")
+    except BladWCiele:
+        print("Niepoprawnie podana zawartość partytury. Utworzona partytura jest wadliwa.")
+        raise ValueError("Niepoprawne wczytanie danych. Problem z ciałem pliku.")
