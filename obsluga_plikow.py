4c8d52778acc54e3040c51a8e57aa13417113ea0
fussz <filip@DESKTOP-HE0U923>
2024-02-27 20:12:36 +0100

    Poprawa obslugi wyjatkow w obsludze plikow

--- obsluga_plikow.py
+++ obsluga_plikow.py
@@ -23,31 +23,40 @@ class BladWCiele(Exception):
 
 
 def utworz_partyture(plik: TextIO) -> partytura.Partytura:
-    nowe_metrum: str = plik.readline().replace('\n', '').replace(' ', '')
-    nowa_liczba_taktow: int = int(plik.readline().replace('\n', '').replace(' ', ''))
-    nowa_tonacja: str = plik.readline().replace('\n', '').replace(' ', '')
-    return partytura.Partytura(tonacja.Tonacja(nowa_tonacja), enum_metrum.Metrum(nowe_metrum), nowa_liczba_taktow)
+    try:
+        nowe_metrum: str = plik.readline().replace('\n', '').replace(' ', '')
+        nowa_liczba_taktow: int = int(plik.readline().replace('\n', '').replace(' ', ''))
+        nowa_tonacja: str = plik.readline().replace('\n', '').replace(' ', '')
+        return partytura.Partytura(tonacja.Tonacja(nowa_tonacja), enum_metrum.Metrum(nowe_metrum), nowa_liczba_taktow)
+    except TypeError:
+        print("Blad co do naglowka partytury")
+        raise BladWNaglowku
 
 
 def wypelnij_partyture_akordami(plik: TextIO, nowa_partytura: partytura.Partytura) -> partytura.Partytura:
-    for linia in plik:
-        linia = linia.replace(' ', '').replace('\n', '')
-        if linia == "T":
-            nowa_partytura.zakoncz_takt()
-        else:
-            podane_dzwieki = linia.split(',')
+    try:
+        for linia in plik:
+            linia = linia.replace(' ', '').replace('\n', '')
+            if linia == "T":
+                nowa_partytura.zakoncz_takt()
+            else:
+                podane_dzwieki = linia.split(',')
 
-            dzwiek_sopranu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[0][-1]),
-                                                          podane_dzwieki[0][0:-1].strip())
-            dzwiek_altu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[1][-1]), podane_dzwieki[1][0:-1].strip())
-            dzwiek_tenoru: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[2][-1]),
-                                                         podane_dzwieki[2][0:-1].strip())
-            dzwiek_basu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[3][-1]), podane_dzwieki[3][0:-1].strip())
+                dzwiek_sopranu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[0][-1]),
+                                                              podane_dzwieki[0][0:-1].strip())
+                dzwiek_altu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[1][-1]), podane_dzwieki[1][0:-1].strip())
+                dzwiek_tenoru: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[2][-1]),
+                                                             podane_dzwieki[2][0:-1].strip())
+                dzwiek_basu: dzwiek.Dzwiek = dzwiek.Dzwiek(int(podane_dzwieki[3][-1]), podane_dzwieki[3][0:-1].strip())
 
-            nowy_akord: akord.Akord = akord.Akord(dzwiek_sopranu, dzwiek_altu, dzwiek_tenoru, dzwiek_basu,
-                                                  int(podane_dzwieki[4]))
-            nowa_partytura.dodaj_akord(nowy_akord)
-    return nowa_partytura
+                nowy_akord: akord.Akord = akord.Akord(dzwiek_sopranu, dzwiek_altu, dzwiek_tenoru, dzwiek_basu,
+                                                      int(podane_dzwieki[4]))
+                nowa_partytura.dodaj_akord(nowy_akord)
+        return nowa_partytura
+    except IndexError:
+        raise BladWCiele()
+    except TypeError:
+        raise BladWCiele()
 
 
 def odczytuj_plik(sciezka_do_pliku: str) -> partytura.Partytura:
